4447be1a69fda59108b23e683fa5e980aa2fc730
Eran Geva <eran.geva@gmail.com>
2023-06-21 11:51:13 +0300

    last_check_alive.txt

--- checkAlive.sh
+++ checkAlive.sh
@@ -1,12 +1,14 @@
 # echo hello
 cd $( realpath $(dirname -- "$0") )
-# ps ax | grep -v grep | grep "python -m RedAlert"
+
+LOG_FILE=./last_check_alive.txt
+echo $(date) > $LOG_FILE
+ps ax | grep -v grep | grep "python -m RedAlert" >> $LOG_FILE
 
 if ! ps ax | grep -v grep | grep --quiet "python -m RedAlert"
 then
+    echo "starting new" >> $LOG_FILE
     (pipenv run python -m RedAlert --posts &)
-    # (pipenv shell && python3 -m RedAlert --posts &)
-    # echo not running
-# else
-#     echo running
+else
+    echo "all is good not starting" >> $LOG_FILE
 fi
