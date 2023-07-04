d8067264598e1957f63e29dfb9d9edbb343c01ca
Eran Geva <eran.geva@gmail.com>
2023-07-04 15:59:31 +0300

    limit size of error msg

--- RedAlert.py
+++ RedAlert.py
@@ -40,6 +40,7 @@ class RedAlert:
             return
         except Exception as ex:
             msg = f"Crush because of {type(ex).__name__}: {ex}\n{''.join(traceback.format_exc())}\n{self.errors_to}"
+            msg = msg[:450]
             print(msg)  # , ''.join(traceback.format_exception(e)))
             self.masto.mastodon.status_post(msg, in_reply_to_id=None, visibility="direct")
             raise
